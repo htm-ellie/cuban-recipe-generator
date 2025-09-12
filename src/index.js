342d5a657215d095e792a9628d97cfdb36cef9ce
htm-ellie <html.elitt@gmail.com>
2025-09-12 12:44:41 -0400

    added the API
    having trouble with the styling of the response. Need to walk away for now, will work on next time.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-function generateRecipe(event) {
-  event.preventDefault();
-
+function displayRecipe(response) {
+  console.log("recipe generated");
   new Typewriter("#recipe-response", {
-    strings: "Ropa Vieja Recipe",
+    strings: response.data.answer,
     autoStart: true,
     delay: 10,
     cursor: "",
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+  let receipeQuery = document.querySelector("#recipe-query");
+  let apiKey = "84ob6879a3t305ff0c4b95aea130d3b6";
+  let context =
+    "You are a chef specializing in traditional cuban cuisine. Provide one recipe using the imperial system of measurements. Make sure to create a recipe based on the user dish from the user instructions. format your response with an ingredients list and an instructions list, for example <p>these are the ingredients</p> </br> <p>these are the instructions</p>. seperate each line with a <br /> and provide the ingredients and the instructions in paragraph form.";
+  let prompt = `User instructions: generate a traditional cuban recipe for ${receipeQuery.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating recipe");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");
