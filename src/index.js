e994129ba7df45e52c5df44e96cd01a8b0680c32
htm-ellie <html.elitt@gmail.com>
2025-09-15 13:35:27 -0400

    updated the User experience
    added hint, generating recipe message, and hidden recipe response box

--- src/index.js
+++ src/index.js
@@ -10,12 +10,16 @@ function displayRecipe(response) {
 
 function generateRecipe(event) {
   event.preventDefault();
-  let receipeQuery = document.querySelector("#recipe-query");
+  let recipeQuery = document.querySelector("#recipe-query");
   let apiKey = "84ob6879a3t305ff0c4b95aea130d3b6";
   let context =
     "You are an AI chef specializing in traditional cuban cuisine. Provide one recipe using the imperial system of measurements. Make sure to create a recipe based on the user dish from the user instructions. format your response with an ingredients list a simplified instructions list, and ingredients list. Please generate a recipe in HTML, and only provide the basic HTML. Do not include ```html at the beginning or ``` at the end.";
-  let prompt = `User instructions: generate a traditional cuban recipe for ${receipeQuery.value}`;
+  let prompt = `User instructions: generate a traditional cuban recipe for ${recipeQuery.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let recipeResponse = document.querySelector("#recipe-response");
+  recipeResponse.classList.remove("hidden");
+  recipeResponse.innerHTML = `<div class="generating">Generating recipe for ${recipeQuery.value}..</div>`;
 
   console.log("Generating recipe");
   console.log(`Prompt: ${prompt}`);
